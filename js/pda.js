365066908312039bc9e1a05f4f864f14626858d3
tavo1987 <tavo198718@gmail.com>
2015-10-02 13:14:25 -0500

    Nuevos estilos

--- js/pda.js
+++ js/pda.js
@@ -16,22 +16,26 @@ jQuery(document).ready(function($) {
 		event.preventDefault();
 
 		$('.norte  .project').toggleClass('active animated zoomInDown');
+		$('.preguntas').hide();
 		
 	});
 
 	$('.zona-centro').on('click', function(even) {
 		even.preventDefault();
 		$('.centro  .project').toggleClass('active animated zoomInDown');
+		$('.preguntas').hide();
 	});
 
 	$('.zona-costa').on('click', function(even) {
 		even.preventDefault();
 		$('.costa  .project').toggleClass('active animated zoomInDown');
+		$('.preguntas').hide();
 	});
 
 	$('.zona-sur').on('click', function(even) {
 		even.preventDefault();
 		$('.sur  .project').toggleClass('active animated zoomInDown');
+		$('.preguntas').hide();
 	});
 
 
@@ -68,12 +72,16 @@ jQuery(document).ready(function($) {
 
 		$('.project__content.carchi').css({'opacity':'1','z-index':'40',});
 		$('.project__content.esmeraldas').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-esmeraldas').removeClass('current');
 
 	});//fin carchi
 
 	$('.btn-esmeraldas').on('click', function() {
 		$('.project__content.esmeraldas').css({'opacity':'1','z-index':'40'});
 		$('.project__content.carchi').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-carchi').removeClass('current');
 	});
 
 	
@@ -84,26 +92,35 @@ jQuery(document).ready(function($) {
 	$('.btn-los-rios').on('click', function() {
 		$('.project__content.los-rios').css({'opacity':'1','z-index':'40',});
 		$('.project__content.manabi').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-manabi').removeClass('current');
 	});
 
 	$('.btn-manabi').on('click', function() {
 		$('.project__content.manabi').css({'opacity':'1','z-index':'40',});
-		 $('.project__content.los-rios').css({'opacity':'0','z-index':'0'});
+		$('.project__content.los-rios').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-los-rios').removeClass('current');
 	});
 
 
 
 	/*===========================================================================
-	/*BOTONES PARA ABRIR PROYECTOS ZONA COSTA
+	/*BOTONES PARA ABRIR PROYECTOS ZONA CENTRO
 	/*===========================================================================*/
 	$('.btn-tungurahua').on('click', function() {
 		$('.project__content.tungurahua').css({'opacity':'1','z-index':'40',});
 		$('.project__content.cotopaxi').css({'opacity':'0','z-index':'0'});
+		
+		$(this).addClass('current');
+		$('.btn-cotopaxi').removeClass('current');
 	});
 
 	$('.btn-cotopaxi').on('click', function() {
 		$('.project__content.cotopaxi').css({'opacity':'1','z-index':'40',});
 		$('.project__content.tungurahua').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-tungurahua').removeClass('current');
 	});
 
 
@@ -113,11 +130,16 @@ jQuery(document).ready(function($) {
 	$('.btn-chimborazo').on('click', function() {
 		$('.project__content.chimborazo').css({'opacity':'1','z-index':'40',});
 		$('.project__content.bolivar').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-bolivar').removeClass('current');
+
 	});
 
 	$('.btn-bolivar').on('click', function() {
 		$('.project__content.bolivar').css({'opacity':'1','z-index':'40',});
 		$('.project__content.chimborazo').css({'opacity':'0','z-index':'0'});
+		$(this).addClass('current');
+		$('.btn-chimborazo').removeClass('current');
 	});
 
 
